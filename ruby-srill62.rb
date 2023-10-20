3644b13a624ef13df670f0fb1dcc029918f633eb
motooga <127541957+motooga@users.noreply.github.com>
2023-10-21 05:58:33 +0900

    Create ruby-srill62.rb

---
+++ ruby-srill62.rb
@@ -0,0 +1,194 @@
+# Rubyのドリルを解いていく
+### 課題
+前回作成したアプリケーションに機能を追加しましょう。
+#### 前回までのコード
+
+```ruby
+def register_data(cars)
+  puts "車種の入力をして下さい。"
+  type = gets.chomp
+  puts "1Lあたりの走行可能距離(km/l)を入力して下さい。"
+  fuel_economy = gets.to_f
+  puts "乗車可能人数を入力して下さい。"
+  capacity = gets.to_i
+  car = { type: type, fuel_economy: fuel_economy, capacity: capacity }
+  cars << car
+end
+
+def show_cars(cars)
+  cars.each_with_index do |car, index|
+    puts "[#{index}]: #{car[:type]}"
+  end
+  puts "確認したい番号を入力して下さい。"
+  input = gets.to_i
+  car = cars[input]
+  if car
+    show_data(car)
+  else
+    puts "該当する番号はありません。"
+  end
+end
+
+def show_data(car)
+  puts car[:type]
+  puts "1Lあたりの走行可能距離:#{car[:fuel_economy]}km/l"
+  puts "乗車人数:#{car[:capacity]}人"
+end
+
+cars = []
+
+while true do
+  puts "番号を入力して下さい"
+  puts "[0]:登録をする"
+  puts "[1]:データを確認する"
+  puts "[2]:終了する"
+  input = gets.chomp
+
+  case input
+  when "0"
+    register_data(cars)
+  when "1"
+    show_cars(cars)
+  when "2"
+    exit
+  else
+    puts "無効な値です"
+  end
+end
+```
+
+### 追加する仕様
+- [1]の処理
+これから走る距離(km)をユーザーに入力を行って貰う
+これから走る距離に対して何L(リットル)ガソリンを消費するか出力を行う
+
+### 回答
+
+```ruby
+def register_data(cars)
+  puts "車種の入力をして下さい。"
+  type = gets.chomp
+  puts "1Lあたりの走行可能距離(km/l)を入力して下さい。"
+  fuel_economy = gets.to_f
+  puts "乗車可能人数を入力して下さい。"
+  capacity = gets.to_i
+  car = { type: type, fuel_economy: fuel_economy, capacity: capacity }
+  cars << car
+  puts "これから走る距離を入力してください(km)"
+  input = gets.to_i
+  puts "使用するガソリンは#{(input / fuel_economy).round(2)}Lです。"
+end
+
+def show_cars(cars)
+  cars.each_with_index do |car, index|
+    puts "[#{index}]: #{car[:type]}"
+  end
+  puts "確認したい番号を入力して下さい。"
+  input = gets.to_i
+  car = cars[input]
+  if car
+    show_data(car)
+  else
+    puts "該当する番号はありません。"
+  end
+end
+
+def show_data(car)
+  puts car[:type]
+  puts "1Lあたりの走行可能距離:#{car[:fuel_economy]}km/l"
+  puts "乗車人数:#{car[:capacity]}人"
+end
+
+cars = []
+
+while true do
+  puts "番号を入力して下さい"
+  puts "[0]:登録をする"
+  puts "[1]:データを確認する"
+  puts "[2]:終了する"
+  input = gets.chomp
+
+  case input
+  when "0"
+    register_data(cars)
+  when "1"
+    show_cars(cars)
+  when "2"
+    exit
+  else
+    puts "無効な値です"
+  end
+end
+```
+
+### 模範解答
+
+```ruby
+def register_data(cars)
+  puts "車種の入力をして下さい。"
+  type = gets.chomp
+  puts "1Lあたりの走行可能距離(km/l)を入力して下さい。"
+  fuel_economy = gets.to_f
+  puts "乗車可能人数を入力して下さい。"
+  capacity = gets.to_i
+  car = { type: type, fuel_economy: fuel_economy, capacity: capacity }
+  cars << car
+end
+
+def show_cars(cars)
+  cars.each_with_index do |car, index|
+    puts "[#{index}]: #{car[:type]}"
+  end
+  puts "確認したい番号を入力して下さい。"
+  input = gets.to_i
+  car = cars[input]
+  if car
+    show_data(car)
+  else
+    puts "該当する番号はありません。"
+  end
+end
+
+def show_data(car)
+  puts "車種:#{car[:type]}"
+  puts "1Lあたりの走行可能距離:#{car[:fuel_economy]}km/l"
+  puts "乗車人数:#{car[:capacity]}人"
+
+  puts "これから走る距離(km)を入力して下さい。"
+  input_distance = gets.to_f
+
+  calculate_fuel_consumption(car, input_distance)
+end
+
+def calculate_fuel_consumption(car, distance)
+  fuel_consumption = distance / car[:fuel_economy]
+  puts "その目的地までは、ガソリンを#{fuel_consumption.round(1)}L消費します。"
+end
+
+cars = []
+
+while true do
+  puts "番号を入力して下さい"
+  puts "[0]:登録をする"
+  puts "[1]:データを確認する"
+  puts "[2]:終了する"
+  input = gets.chomp
+
+  case input
+  when "0"
+    register_data(cars)
+  when "1"
+    show_cars(cars)
+  when "2"
+    exit
+  else
+    puts "無効な値です"
+  end
+end
+```
+
+### レビュー
+- [1]の処理に追加するのに[0]の処理に追加してしまった
+- caluculate_fuel_comsumptionと新たなメソッドを作成して処理を書く方が良い
+- その時に(car, distance)を引数で渡すようにする
+
